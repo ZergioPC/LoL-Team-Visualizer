12d95cc7eb422bd17eaff45470a72aed9d73ffbe
Sergio Palacios 2 <sergid26pal@gmail.com>
2026-01-03 23:45:25 -0500

    FEATURE. Se muestran los campeones con su avatar, nombre y habilidades

--- src/components/LolChampItem/index.jsx
+++ src/components/LolChampItem/index.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import UseApiEndpoints from "../../hooks/UseApiEndpoints";
 
 const LolChampItem = ({ champ })=>{
   //console.log(champ);
   
+  const { champImg:URL } =  UseApiEndpoints();
   return(
     <article>
-      {champ.name}
+      <section>
+        <h3>{champ.name}</h3>
+      </section>
+      <figure>
+        <img src={URL + champ.image.full} alt={champ.id + "_avatar"} />
+      </figure>
+      <ul>
+        {champ.spells.map(spell => (
+          <li key={spell.id}>{spell.name}</li>
+        ))}
+      </ul>
     </article>
   );
 }
